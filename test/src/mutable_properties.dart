5c536f76ff57e81e65d1aae4b98d665a289db070
Alexander Gutev <alex.gutev@gmail.com>
2024-01-30 19:01:59 +0100

    Add unit tests for CellExtension with mutable: true.

---
+++ test/src/mutable_properties.dart
@@ -0,0 +1,66 @@
+import 'package:live_cells/live_cell_annotations.dart';
+import 'package:source_gen_test/annotations.dart';
+
+// Test that an extension is generated for every public and final property of a class.
+// Also tests that an extension on MutableCell is generated with accessors
+// for every field in the constructor
+
+@ShouldGenerate(
+    r'''
+// Extends ValueCell with accessors for Person properties
+extension PersonCellExtension on ValueCell<Person> {
+  ValueCell<String> get firstName => apply((value) => value.firstName);
+  ValueCell<String> get lastName => apply((value) => value.lastName);
+  ValueCell<int> get age => apply((value) => value.age);
+  ValueCell<String> get fullName => apply((value) => value.fullName);
+}
+
+// Extends MutableCell with accessors for Person properties
+extension PersonMutableCellExtension on MutableCell<Person> {
+  static Person _copyWith(
+    Person instance, {
+    String? firstName,
+    String? lastName,
+    int? age,
+  }) {
+    return Person(
+      firstName: firstName ?? instance.firstName,
+      lastName: lastName ?? instance.lastName,
+      age: age ?? instance.age,
+    );
+  }
+
+  MutableCell<String> get firstName =>
+      [this].mutableComputeCell(() => value.firstName, (p) {
+        value = _copyWith(value, firstName: p);
+      });
+  MutableCell<String> get lastName =>
+      [this].mutableComputeCell(() => value.lastName, (p) {
+        value = _copyWith(value, lastName: p);
+      });
+  MutableCell<int> get age => [this].mutableComputeCell(() => value.age, (p) {
+        value = _copyWith(value, age: p);
+      });
+}
+'''
+)
+@CellExtension(mutable: true)
+class Person {
+  final String firstName;
+  final String lastName;
+  final int age;
+
+  String get fullName => '$firstName $lastName';
+
+  // Accessors not generated for variable and private properties
+
+  String? address;
+  final int _id;
+
+  Person({
+    required this.firstName,
+    required this.lastName,
+    required this.age,
+    int id = 0,
+  }) : _id = id;
+}
