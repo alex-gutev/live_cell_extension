59f0f0344ff329548204fde8e8587aab12301e45
Alexander Gutev <alex.gutev@gmail.com>
2024-01-30 19:02:10 +0100

    Add unit test runner.

---
+++ test/live_cell_extension_test.dart
@@ -0,0 +1,22 @@
+import 'package:live_cell_extension/src/cell_extension_generator.dart';
+import 'package:source_gen_test/source_gen_test.dart';
+
+Future<void> main() async {
+  initializeBuildLogTracking();
+
+  testAnnotatedElements(
+    await initializeLibraryReaderForDirectory(
+        'test/src',
+        'computed_properties.dart'
+    ),
+    CellExtensionGenerator(),
+  );
+
+  testAnnotatedElements(
+    await initializeLibraryReaderForDirectory(
+        'test/src',
+        'mutable_properties.dart'
+    ),
+    CellExtensionGenerator(),
+  );
+}
