e93dfc0b627a2397b3f03cde37943f5554ee7b66
Alexander Gutev <alex.gutev@gmail.com>
2025-09-03 16:02:06 +0200

    Update ClassPropVisitor to use Element2.

--- lib/src/class_prop_visitor.dart
+++ lib/src/class_prop_visitor.dart
@@ -1,10 +1,10 @@
 import 'dart:collection';
 
-import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/visitor.dart';
+import 'package:analyzer/dart/element/element2.dart';
+import 'package:analyzer/dart/element/visitor2.dart';
 
 /// Visits a class and extracts information about its properties
-class ClassPropVisitor extends SimpleElementVisitor<void> {
+class ClassPropVisitor extends SimpleElementVisitor2<void> {
   /// If true only publicly accessible properties are included in [fields].
   final bool publicOnly;
 
@@ -15,20 +15,20 @@ class ClassPropVisitor extends SimpleElementVisitor<void> {
   final bool includeSynthetic;
 
   /// The properties of the class which can be accessed
-  UnmodifiableListView<FieldElement> get fields =>
+  UnmodifiableListView<FieldElement2> get fields =>
       UnmodifiableListView(_fields);
 
   /// The properties of the class which can be initialized in the constructor
-  UnmodifiableListView<FieldElement> get mutableFields =>
+  UnmodifiableListView<FieldElement2> get mutableFields =>
       UnmodifiableListView(_mutableFields);
 
   /// The constructor to use when creating an instance of the class
-  ConstructorElement? get constructor => _constructor;
+  ConstructorElement2? get constructor => _constructor;
 
-  final List<FieldElement> _fields = [];
-  final List<FieldElement> _mutableFields =[];
+  final List<FieldElement2> _fields = [];
+  final List<FieldElement2> _mutableFields =[];
 
-  ConstructorElement? _constructor;
+  ConstructorElement2? _constructor;
 
   ClassPropVisitor({
     this.publicOnly = true,
@@ -37,34 +37,34 @@ class ClassPropVisitor extends SimpleElementVisitor<void> {
   });
 
   @override
-  void visitConstructorElement(ConstructorElement element) {
-    if (element.name.isEmpty) {
+  void visitConstructorElement(ConstructorElement2 element) {
+    if (element.name3 == 'new') {
       _constructor = element;
 
-      for (final param in element.parameters) {
+      for (final param in element.formalParameters) {
         _addConstructorParam(param);
       }
     }
   }
 
   /// Add the fields associated with a constructor parameter
-  void _addConstructorParam(ParameterElement param) {
-    if (param.isInitializingFormal && param is FieldFormalParameterElement) {
-      if (param.field != null) {
-        _mutableFields.add(param.field!);
+  void _addConstructorParam(FormalParameterElement param) {
+    if (param.isInitializingFormal && param is FieldFormalParameterElement2) {
+      if (param.field2 != null) {
+        _mutableFields.add(param.field2!);
       }
     }
-    else if (param.isSuperFormal && param is SuperFormalParameterElement) {
-      if (param.superConstructorParameter != null) {
-        _addConstructorParam(param.superConstructorParameter!);
+    else if (param.isSuperFormal && param is SuperFormalParameterElement2) {
+      if (param.superConstructorParameter2 != null) {
+        _addConstructorParam(param.superConstructorParameter2!);
       }
     }
   }
 
   @override
-  void visitFieldElement(FieldElement element) {
+  void visitFieldElement(FieldElement2 element) {
     if ((!publicOnly || element.isPublic) &&
-        (!immutableOnly || element.setter == null) &&
+        (!immutableOnly || element.setter2 == null) &&
         (includeSynthetic || !element.isSynthetic)
         && !element.isStatic) {
       _fields.add(element);
